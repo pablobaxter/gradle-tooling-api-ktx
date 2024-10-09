9a7c040ed3fe684efcb3ab6104b2257fb76a955b
Pablo Baxter <pablobaxter@msn.com>
2024-10-09 00:11:43 -0700

    Complete changes, add in license

--- gradle-tooling-api-ktx/src/main/kotlin/com/frybits/gradle/tooling/ktx/gradleToolingKtx.kt
+++ gradle-tooling-api-ktx/src/main/kotlin/com/frybits/gradle/tooling/ktx/gradleToolingKtx.kt
@@ -1,3 +1,22 @@
+/*
+ *  Copyright 2024 Pablo Baxter
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * Created by Pablo Baxter (Github: pablobaxter)
+ * https://github.com/pablobaxter/gradle-tooling-api-ktx
+ */
+
 package com.frybits.gradle.tooling.ktx
 
 import kotlinx.coroutines.suspendCancellableCoroutine
@@ -13,7 +32,6 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
 suspend fun <T> ModelBuilder<T>.await(): T = suspendCancellableCoroutine { cont ->
-    this.
     get(CoroutineResultHandler(cont))
 }
 
@@ -21,31 +39,28 @@ suspend fun <T> BuildActionExecuter<T>.await() : T = suspendCancellableCoroutine
     run(CoroutineResultHandler(cont))
 }
 
-suspend fun BuildLauncher.await() = suspendCancellableCoroutine { cont ->
+suspend fun BuildLauncher.await(): Void = suspendCancellableCoroutine { cont ->
     run(CoroutineResultHandler(cont))
 }
 
-inline fun <reified T> ProjectConnection.model() : T {
+inline fun <reified T> ProjectConnection.getModel() : T {
     return getModel(T::class.java)
 }
 
-suspend inline fun <reified T> ProjectConnection.getModel() : T = suspendCancellableCoroutine { cont ->
-    getModel(T::class.java, object : ResultHandler<T> {
-        override fun onComplete(result: T) {
-            cont.resume(result)
-        }
-
-        override fun onFailure(e: GradleConnectionException) {
-            cont.resumeWithException(e)
-        }
-    })
+suspend inline fun <reified T> ProjectConnection.awaitModel() : T = suspendCancellableCoroutine { cont ->
+    getModel(T::class.java, CoroutineResultHandler(cont))
 }
 
-suspend fun TestLauncher.await() = suspendCancellableCoroutine { cont ->
+suspend fun TestLauncher.await(): Void = suspendCancellableCoroutine { cont ->
     run(CoroutineResultHandler(cont))
 }
 
-private class CoroutineResultHandler<T>(private val continuation: Continuation<T>) : ResultHandler<T> {
+inline fun <reified T> ProjectConnection.model() : ModelBuilder<T> {
+    return model(T::class.java)
+}
+
+@PublishedApi
+internal class CoroutineResultHandler<T>(private val continuation: Continuation<T>) : ResultHandler<T> {
     override fun onComplete(result: T) {
         continuation.resume(result)
     }
